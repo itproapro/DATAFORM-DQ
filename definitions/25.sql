f263bf2956e72130aab3b26ce89a98261dadd37b
Julian Esteban Garcia <julianesteban.garcia@proapro.es>
2026-03-04 11:50:04 +0000

    reglas

---
+++ definitions/25.sql
@@ -0,0 +1,134 @@
+--query_ump_var_VABME
+
+-- DECLARO VARIABLES
+DECLARE v_rule_id INT64 ;  -- ID de la regla
+DECLARE v_query STRING; -- Consulta de validación, aquí va la lógica de la regla
+DECLARE v_total INT64; -- Total de valores
+DECLARE v_failed INT64; -- Valores que no cumplen con la regla
+DECLARE v_passed FLOAT64; --% calculado entre valores totales y los que no cumplen la regla
+DECLARE v_status STRING; --Status del resultado
+DECLARE v_details STRING; --Detalles de la ejecución
+DECLARE file_name STRING; --Nombre del archivo al bucket
+
+-- DEFINIR ID RULE
+SET v_rule_id = 25;
+
+-- DEFINIR LA REGLA ASIGNADA A v_query: -- UMP_VAR--> este campo indica 1 cuando la unidad de medida base es UNIDAD. 
+SET v_query = '''
+  WITH base AS(
+SELECT
+   MATNR,
+   MTART,
+   VABME,
+   MEINS
+  FROM `cf-esproapro-bic-pro-ou.SH_STG.bqt_material_attr`
+),
+eval AS(
+  SELECT
+    *,
+    CASE
+      WHEN MTART IN ('ZMER','ZSEC','ZFRE') AND VABME = '2' THEN 1
+      WHEN MTART IN ('ZMER','ZSEC','ZFRE') AND MEINS = 'ST' AND VABME IS NULL THEN 1
+      ELSE NULL
+    END AS incorrectos
+  FROM base
+)
+ SELECT
+  COUNTIF(incorrectos = 1) AS materiales_incorrectos FROM eval;
+''';
+
+
+
+-- CALCULAR TOTAL DE REGISTROS E INCUMPLIMIENTOS
+-- Calculo los valores totales
+SET v_total = (SELECT
+  COUNT(*)
+  FROM `cf-esproapro-bic-pro-ou.SH_STG.bqt_material_attr`
+WHERE MTART IN ('ZMER','ZSEC','ZFRE'));
+-- Calculo los valores que no cumplen la condición y los asigno a v_failed
+EXECUTE IMMEDIATE v_query INTO v_failed;
+
+IF v_failed IS NULL THEN
+  SET v_failed = 0;
+END IF;
+
+-- CALCULAR PORCENTAJE COMPLETITUD
+
+SET v_passed = ROUND(COALESCE((1 - SAFE_DIVIDE(v_failed, v_total))*100,100),2);
+
+
+
+-- ESTABLECER STATUS: de acuerdo a lo que establescamos, valores críticos tienen que ser 100%
+SET v_status = CASE
+    WHEN v_passed > 99.5  THEN 'PASSED'
+    else 'FAILED'
+END;
+
+-- DETALLES (opcional), aqui pongamos lo que vemaos que aporta
+SET v_details = CONCAT('Total: ', v_total, ', Failed: ', v_failed, ' UMP_VAR: tengo 2 materiales con valor 2, 12420;17882');
+
+-- INSERTAR RESULTADO EN LA TABLA
+INSERT INTO cf-esproapro-bic-pro-ou.SH_REP.FACT_DQ_RESULTS (
+    RESULT_ID,
+    RULE_ID,
+    EXECUTION_TIME,
+    TOTAL_RECORDS,
+    FAILED_RECORDS,
+    PASSED_PERCENTAGE,
+    STATUS,
+    DETAILS
+)
+VALUES (
+    GENERATE_UUID(), --creo ID aleatorio para cada ejecución
+    v_rule_id,
+    CURRENT_TIMESTAMP(),
+    v_total,
+    v_failed,
+    v_passed,
+    v_status,
+    v_details
+);
+
+
+
+IF v_status = 'FAILED' THEN
+SET file_name = CONCAT(
+  'gs://maestromateriales-dataquality-pap/REGLA_DQ_25_MARA_',
+  FORMAT_TIMESTAMP('%Y%m%d_%H%M%S', CURRENT_TIMESTAMP()),
+  '_*.csv'
+);
+
+EXECUTE IMMEDIATE FORMAT("""
+  EXPORT DATA OPTIONS (
+    uri = '%s',
+    format = 'CSV',
+    header = true,
+    field_delimiter = ';',
+    overwrite = true
+  )
+  AS
+   WITH base AS(
+SELECT
+   MATNR,
+   MTART,
+   VABME,
+   MEINS
+  FROM `cf-esproapro-bic-pro-ou.SH_STG.bqt_material_attr`
+),
+eval AS(
+  SELECT
+    *,
+    CASE
+      WHEN MTART IN ('ZMER','ZSEC','ZFRE') AND VABME = '2' THEN 1
+      WHEN MTART IN ('ZMER','ZSEC','ZFRE') AND MEINS = 'ST' AND VABME IS NULL THEN 1
+      ELSE NULL
+    END AS incorrectos
+  FROM base
+)
+ SELECT
+  * 
+  FROM eval
+  ORDER BY MATNR;
+""", file_name);
+
+END IF;
---
+++ definitions/3.sql
@@ -0,0 +1,119 @@
+--query_tipo_material_MTART
+
+-- DECLARO VARIABLES
+DECLARE v_rule_id INT64 ;  -- ID de la regla
+DECLARE v_query STRING; -- Consulta de validación, aquí va la lógica de la regla
+DECLARE v_total INT64; -- Total de valores
+DECLARE v_failed INT64; -- Valores que no cumplen con la regla
+DECLARE v_passed FLOAT64; --% calculado entre valores totales y los que no cumplen la regla
+DECLARE v_status STRING; --Status del resultado
+DECLARE v_details STRING; --Detalles de la ejecución
+DECLARE file_name STRING; --Nombre del archivo al bucket
+
+-- DEFINIR ID RULE
+SET v_rule_id = 3;
+
+-- DEFINIR LA REGLA ASIGNADA A v_query: TIPO DE MATERIAL según MATNR
+SET v_query = '''
+    WITH base AS (
+  SELECT
+    MATNR,
+    MTART,
+    LEFT(RIGHT(CAST(MATNR AS STRING),5),1)AS d5
+  FROM `cf-esproapro-bic-pro-ou.SH_STG.bqt_material_attr`
+),
+eval AS (
+  SELECT 
+    *,
+     CASE
+      WHEN d5 = '1' AND MTART = 'ZMER' THEN 1
+      WHEN d5 = '3' AND MTART = 'ZFRE' THEN 1
+      WHEN d5 = '4' AND MTART = 'ZSEC' THEN 1
+      WHEN d5 NOT IN ('1', '3', '4') AND MTART IN ('ZMER','ZFRE','ZSEC') THEN 0
+      ELSE NULL
+     END AS cumple
+  FROM base
+)
+SELECT 
+  COUNTIF(cumple = 0) AS materiales_incorrectos
+FROM eval;
+''';
+
+
+
+-- CALCULAR TOTAL DE REGISTROS E INCUMPLIMIENTOS
+-- Calculo los valores totales
+SET v_total = (SELECT COUNT(*) FROM cf-esproapro-bic-pro-ou.SH_STG.bqt_material_attr);
+-- Calculo los valores que no cumplen la condición y los asigno a v_failed
+EXECUTE IMMEDIATE v_query INTO v_failed;
+
+IF v_failed IS NULL THEN
+  SET v_failed = 0;
+END IF;
+
+-- CALCULAR PORCENTAJE COMPLETITUD
+SET v_passed = ROUND(COALESCE((1 - SAFE_DIVIDE(v_failed, v_total))*100,100),2);
+
+
+-- ESTABLECER STATUS: de acuerdo a lo que establescamos, valores críticos tienen que ser 100%
+SET v_status = CASE
+    WHEN v_passed > 99.5  THEN 'PASSED'
+    else 'FAILED'
+END;
+-- DETALLES (opcional), aqui pongamos lo que vemaos que aporta
+SET v_details = CONCAT('Total: ', v_total, ', Failed: ', v_failed, ' TIPO MATERIAL: ZMER-ZFRE-ZSEC');
+
+-- INSERTAR RESULTADO EN LA TABLA
+INSERT INTO cf-esproapro-bic-pro-ou.SH_REP.FACT_DQ_RESULTS (
+    RESULT_ID,
+    RULE_ID,
+    EXECUTION_TIME,
+    TOTAL_RECORDS,
+    FAILED_RECORDS,
+    PASSED_PERCENTAGE,
+    STATUS,
+    DETAILS
+)
+VALUES (
+    GENERATE_UUID(), --creo ID aleatorio para cada ejecución
+    v_rule_id,
+    CURRENT_TIMESTAMP(),
+    v_total,
+    v_failed,
+    v_passed,
+    v_status,
+    v_details
+);
+
+
+IF v_status = 'FAILED' THEN
+SET file_name = CONCAT(
+  'gs://maestromateriales-dataquality-pap/REGLA_DQ_3_MARA_',
+  FORMAT_TIMESTAMP('%Y%m%d_%H%M%S', CURRENT_TIMESTAMP()),
+  '_*.csv'
+);
+
+EXECUTE IMMEDIATE FORMAT("""
+  EXPORT DATA OPTIONS (
+    uri = '%s',
+    format = 'CSV',
+    header = true,
+    field_delimiter = ';',
+    overwrite = true
+  )
+  AS
+  WITH base AS (
+  SELECT
+    LPAD(CAST(MATNR AS STRING),18,'0') AS MATNR,
+    NULLIF(TRIM(CAST(MATKL AS STRING)),'') AS MATKL,
+    MTART
+  FROM `cf-esproapro-bic-pro-ou.SH_STG.bqt_material_attr`
+    )
+    SELECT 
+      COUNT(*)
+    FROM base
+    WHERE(
+      (MATKL IS NULL AND MTART IN ('ZMER','ZSEC','ZFRE')) OR (MATKL NOT IN ('SUPPLIER','SYSCO','NO.BRAND','METRO','MAKRO','PAP','CUSTOMER') AND MTART IN ('ZMER','ZSEC','ZFRE')))
+""", file_name);
+
+END IF;
